e83b963fa583684227a270eaf35c16f0d948134a
rogerthemarquis <rogerthemarquis@gmail.com>
2025-02-23 15:11:52 -0600

    feat: added tailwind and light/dar mode toggle

---
+++ RtM/RtM/wwwroot/javascript/theme.js
@@ -0,0 +1,17 @@
+﻿window.toggleDarkMode = (isDark) => {
+    if (isDark) {
+        document.documentElement.classList.add('dark');
+    } else {
+        document.documentElement.classList.remove('dark');
+    }
+};
+
+// Check the user's system preference on initial load
+window.applySystemPreference = () => {
+    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    if (prefersDarkScheme) {
+        document.documentElement.classList.add('dark');
+    } else {
+        document.documentElement.classList.remove('dark');
+    }
+};
