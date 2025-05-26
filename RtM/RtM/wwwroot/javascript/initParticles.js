18e806e5f335118a4b7d83478693bff4fa2d788a
rogerthemarquis <rogerthemarquis@gmail.com>
2025-05-26 16:27:24 -0500

    feat: responsive particles

--- RtM/RtM/wwwroot/javascript/initParticles.js
+++ RtM/RtM/wwwroot/javascript/initParticles.js
@@ -1,7 +1,33 @@
 ﻿window.initParticles = function () {
     if (window.Particles && typeof window.Particles.init === "function") {
         Particles.init({
-            selector: '.background'
+            selector: '.background',
+            responsive: [
+                {
+                    breakpoint:
+                        880
+                    ,
+                    options: {
+                        maxParticles:
+                            70
+                        ,
+                        connectParticles:
+                            true
+                    }
+                },
+                {
+                    breakpoint:
+                        440
+                    ,
+                    options: {
+                        maxParticles:
+                            40
+                        ,
+                        connectParticles:
+                            true
+                    }
+                },
+            ]
         });
     } else {
         console.error("Particles.js is not loaded or not available.");
